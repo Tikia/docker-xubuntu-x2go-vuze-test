88bd9cdec12aacda4cf77a51ed07be2806129a3a
Tikia <Tikia@users.noreply.github.com>
2018-08-24 14:58:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 if [ ! -f /.root_pw_set ]; then
-	/root_pw_set.sh
+	echo "==================================================================="
+	echo "Setting root password to $_ENV['ROOT_PASS']"
+	echo "root:$_ENV['ROOT_PASS']" | chpasswd
+	echo "=> Done!"
+	echo "-------------------------------------------------------------------"
+	echo "Creating user '$_ENV['USER_NAME']' with password $_ENV['USER_PASS']"
+	adduser --disabled-password --gecos "" $_ENV['USER_NAME'] 
+	adduser $_ENV['USER_NAME'] sudo
+	chown -R $_ENV['USER_NAME']:$_ENV['USER_NAME'] /home/$_ENV['USER_NAME']
+	echo "$_ENV['USER_NAME']:$_ENV['USER_PASS']" | chpasswd
+	echo "=> Done!"
+	touch /.root_pw_set
+	echo "==================================================================="
 fi
 
 exec /usr/sbin/sshd -D
